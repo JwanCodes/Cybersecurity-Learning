99f4c86e6c6564abb370efd7f447771f35533381
JwanCodes <jwan.alhumidan@gmail.com>
2025-11-22 20:54:00 +0300

    Implement password strength checker function

--- Projects/password_checker.py
+++ Projects/password_checker.py
@@ -1,4 +1,25 @@
-password = input("Enter a password to check: ")
+import re
 
-# logic will be added later
-print("Password strength evaluation coming soon...")
+def check_password_strength(password):
+    strength = 0
+
+    if len(password) >= 8:
+        strength += 1
+    if re.search(r"[A-Z]", password):
+        strength += 1
+    if re.search(r"[a-z]", password):
+        strength += 1
+    if re.search(r"[0-9]", password):
+        strength += 1
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        strength += 1
+
+    if strength <= 2:
+        return "Weak"
+    elif strength == 3 or strength == 4:
+        return "Medium"
+    else:
+        return "Strong"
+
+password = input("Enter a password to check: ")
+print("Password Strength:", check_password_strength(password))
